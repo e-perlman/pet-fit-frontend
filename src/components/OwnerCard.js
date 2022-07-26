9dbe0d47b0e84e8283cfbc36fa526d671dbb48dd
Emma Perlman <emmajperlman@gmail.com>
2022-07-26 14:22:42 -0700

    ability to delete owner profile on frontend and delete request on backend

--- src/components/OwnerCard.js
+++ src/components/OwnerCard.js
@@ -1,9 +1,17 @@
 
-const OwnerCard = ({owner}) => {
+const OwnerCard = ({owner,onDeleteProfile}) => {
+  const handleClick = () => {onDeleteProfile(owner.id) }
   return (
     <>
         <div>Owner Card</div>
-        <h1>{owner ? `${owner.first_name} ${owner.last_name}` : 'Please Select an Owner!'}</h1>
+        {owner? 
+        <div>
+          <h1>{`${owner.first_name} ${owner.last_name}`}</h1>
+          <h3>{`Age: ${owner.age}`}</h3>
+          <h3>{`Email: ${owner.email}`}</h3>
+          <button onClick={handleClick}>Remove Profile</button>
+        </div>
+        :<h1>Please Select an Owner!</h1>}
     </>
   )
 }
--- src/containers/OwnerProfile.js
+++ src/containers/OwnerProfile.js
@@ -1,15 +1,13 @@
 import { useState,useEffect } from "react"
-import {Route,Switch} from 'react-router-dom';
 import OwnerCard from "../components/OwnerCard"
 import OwnerFilter from "../components/OwnerFilter"
 import PetList from "../components/PetList"
-import PetPage from "../components/PetPage";
-import StatusList from "../components/StatusList"
 
 const OwnerProfile = () => {
     const [owners,setOwners]=useState([])
     const [loading, setLoading] = useState(true)
     const [selectedOwnerId, setSelectedOwnerId]=useState(null)
+
     useEffect(() => {
         fetch("http://127.0.0.1:9393/owners")
         .then((r) => r.json())
@@ -17,6 +15,15 @@ const OwnerProfile = () => {
         setLoading(false)
     }, [])
     if (loading) return <h1>Loading...</h1>
+
+    const removeOwner = (removedOwnerId) => {
+      const updatedOwners=owners.filter(owner=>owner.id!==removedOwnerId)
+      setOwners(updatedOwners)
+      fetch(`http://127.0.0.1:9393/owners/${removedOwnerId}`, {
+      method: "DELETE",
+      })
+    .then((r) => r.json())
+    }
 
     const handleSelectOwner = (e) => { setSelectedOwnerId(e.target.value)}
  
@@ -29,7 +36,7 @@ const OwnerProfile = () => {
     <>
         <div>OwnerProfile</div>
         <OwnerFilter owners={owners} onOwnerChange={handleSelectOwner}/>
-        <OwnerCard owner={selectedOwner}/>
+        <OwnerCard owner={selectedOwner} onDeleteProfile={removeOwner}/>
         {selectedOwner ? <PetList pets={selectedOwner?.pets} interactive={false}/> : null}
 
     </>
