33b7c23600157f2250706fbd907c33543d014b8e
Emma Perlman <emmajperlman@gmail.com>
2022-07-26 13:33:34 -0700

    add more info on pet card

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,14 +1,19 @@
 import { useState , useEffect } from "react"
 import { Link,useParams } from "react-router-dom"
-import StatusCard from "./StatusCard"
-import StatusForm from "./StatusForm"
 
 
 const PetCard = ({pet}) => {
   
   return (
     <div>
-      <h2><Link to={`/pets/${pet.id}`}>{pet.name}</Link></h2>  
+      <h2>{pet.name}</h2>
+      <h4>Species: {pet.species}</h4>
+      <h4>Age: {`${pet.age} years`}</h4>
+      <h4>Sex: {pet.sex}</h4>
+      <h4>Breed: {pet.breed}</h4>
+      <h4>Color: {pet.color}</h4>
+      <h4>Weight: {`${pet.weight} lbs`}</h4>
+      <button><Link to={`/pets/${pet.id}`}>View My Page!</Link></button>  
     </div>
   )
 }
