7532897d6a87cc84f0f0bcc4816f1f03f11351c4
Emma Perlman <emmajperlman@gmail.com>
2022-08-10 14:33:31 -0700

    add font to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
+import { ThemeProvider } from '@emotion/react';
+import { Typography } from '@mui/material';
+import {createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Russo One, sans-serif'
+  }})
 
 const Header = ({title}) => {
   return (
     <div>
-        <h1>{title}</h1>
+      <ThemeProvider theme={theme}>
+        <Typography variant='h2'>{title}</Typography>
+      </ThemeProvider>
     </div>
   )
 }
